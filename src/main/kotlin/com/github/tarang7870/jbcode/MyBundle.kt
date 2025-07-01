897751ad014303156acb0dc8d5abf977c3b59223
GitHub Action <action@github.com>
2025-07-01 10:15:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/tarang7870/jbcode/MyBundle.kt
+++ src/main/kotlin/com/github/tarang7870/jbcode/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tarang7870.jbcode
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/tarang7870/jbcode/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/tarang7870/jbcode/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.tarang7870.jbcode.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
